38739a81764ef85056846e049790ac3a1c0bbeeb
Michael Bade <mbade314@gmail.com>
2020-05-19 00:01:18 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,9 @@ class Post
   attr_accessor :author
   @@all = []
 
-  def initialize(title)
+  def initialize(title, author = nil)
     @title = title
+    @author = author
     @@all << @title
   end
 
